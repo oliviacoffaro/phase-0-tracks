c78e8a6449504b9c358f1aef7aeffd5151457ceb
Olivia Coffaro <ofcoffaro@gmail.com>
2016-08-10 15:06:07 -0400

    adding update opportunity

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -33,7 +33,19 @@ puts "Do you like the color blue? (yes/no)"
 # Print the hash back out to the screen when the designer has answered all of the questions.
   p client
 # Give the user the opportunity to update a key (no need to loop, once is fine).
-  puts "Does that look right to you? (yes/no)"
+  puts "Would you like to change anything? (yes/no)"
+# If the designer says "none", skip it.
+    if gets.chomp == "no"
+      puts "Great! Here is your final client data: "
+      p client
+    elsif gets.chomp == "yes"
+      puts "What would you like to edit?" 
+    end
 
-# If the designer says "none", skip it. But if the designer enters "decor_theme" (for example), your program should ask for a new value and update the :decor_theme key. (Hint: Strings have methods that will turn them into symbols, which would be quite handy here.) You can assume the user will correctly input a key that exists in your hash -- no need to handle user errors.
+#But if the designer enters "decor_theme" 
+#(for example), your program should ask for a new value and update the :decor_theme key. 
+#(Hint: Strings have methods that will turn them into symbols, which would be quite handy here.) 
+#You can assume the user will correctly input a key that exists in your hash -- no need to handle user errors.
 # Print the latest version of the hash, and exit the program.
+
+
