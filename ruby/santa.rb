72a4123e2d1491e8461f52907b322dd5cd3bfd2e
Olivia Coffaro <ofcoffaro@gmail.com>
2016-08-16 16:37:59 -0400

    adding array

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,8 +1,8 @@
 class Santa
-  def initialize
+  def initialize(gender, ethnicity)
     puts "Initializing Santa instance ..."
     @gender = gender
-    @enthnicity = enthnicity
+    @ethnicity = ethnicity
     reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
     age = 0
   end
@@ -17,6 +17,17 @@ class Santa
   
 end
 
-larry = Santa.new
-larry.speak
-larry.eat_milk_and_cookies("chocolate chip")
+# larry = Santa.new
+# larry.speak
+# larry.eat_milk_and_cookies("chocolate chip")
+
+santas = []
+santas << Santa.new("agender", "black")
+santas << Santa.new("female", "Latino")
+santas << Santa.new("bigender", "white")
+santas << Santa.new("male", "Japanese")
+santas << Santa.new("female", "prefer not to say")
+santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+santas << Santa.new("N/A", "N/A")
+
+p santas
