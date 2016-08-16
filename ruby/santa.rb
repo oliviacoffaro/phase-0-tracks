73653de8f088bb8dffada0a7b7d6f091355eb949
Olivia Coffaro <ofcoffaro@gmail.com>
2016-08-16 16:34:42 -0400

    adding initializing instances

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,4 +1,11 @@
 class Santa
+  def initialize
+    puts "Initializing Santa instance ..."
+    @gender = gender
+    @enthnicity = enthnicity
+    reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    age = 0
+  end
 
   def speak
     puts "Ho, ho, ho! Haaaappy Holidays!"
@@ -8,13 +15,8 @@ class Santa
     puts "That was a good #{cookie} cookie!"
   end
   
-  def initialize
-    puts "Initializing Santa instance ..."
-  end
 end
 
 larry = Santa.new
-
 larry.speak
 larry.eat_milk_and_cookies("chocolate chip")
-Santa.initialize
