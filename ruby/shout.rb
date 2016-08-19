5cab862ef992dcc69aba18976ced18221c61b965
Olivia Coffaro <ofcoffaro@gmail.com>
2016-08-19 14:13:01 -0400

    finished release one - added happily yelling code

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,13 @@
 module Shout
-  # we'll put some methods here soon, but this code is fine for now!
+   def self.yell_angrily(words)
+    words + " !!!" + " :("
+  end
+  def self.yelling_happily(words)
+    words + " OH YA, OH YA...because I'm HAPPY!"
+  end
 end
+
+#Driver Code
+
+p Shout.yell_angrily("oh poop")
+p Shout.yelling_happily("meow")
