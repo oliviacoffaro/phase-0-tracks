07784af8023155dc2f2255ef5ff52b8ae7cc04ea
Olivia Coffaro <ofcoffaro@gmail.com>
2016-08-19 14:32:36 -0400

    refactoring to add mixin

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,13 +1,44 @@
 module Shout
-   def self.yell_angrily(words)
-    words + " !!!" + " :("
+
+  def travel(hours)
+    puts "I've traveled far and wide! Over #{hours} hours!"
   end
-  def self.yelling_happily(words)
-    words + " OH YA, OH YA...because I'm HAPPY!"
+
+  def tresure_chest(fav_thing)
+    puts "I hope to find a treasure chest, full of gold, rubies, and #{fav_thing}!"
   end
+
 end
 
-#Driver Code
 
-p Shout.yell_angrily("oh poop")
-p Shout.yelling_happily("meow")
+
+class Pirate
+  include Shout
+end
+
+class Parrot
+  include Shout
+end
+
+instance_pirate = Pirate.new
+instance_pirate.travel(9)
+instance_pirate.tresure_chest("money")
+
+instance_parrot = Parrot.new
+instance_parrot.travel(2)
+instance_parrot.tresure_chest("crackers")
+
+
+# module Shout
+#    def self.yell_angrily(words)
+#     words + " !!!" + " :("
+#   end
+#   def self.yelling_happily(words)
+#     words + " OH YA, OH YA...because I'm HAPPY!"
+#   end
+# end
+
+# #Driver Code
+
+# p Shout.yell_angrily("oh poop")
+# p Shout.yelling_happily("meow")
