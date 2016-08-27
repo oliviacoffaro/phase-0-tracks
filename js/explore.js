0b3ea09413e50e11a4954925751810fe760b0f1f
Olivia Coffaro <ofcoffaro@gmail.com>
2016-08-27 16:57:28 -0400

    initial commit for js 7.1

---
+++ js/explore.js
@@ -0,0 +1,9 @@
+// define a method that takes a word as a parameter and then reverses it
+// input : string
+// output reverse string
+// expect: hello to return olleh
+// 1. function that takes a string as a argurment
+// 2. make a variable that holds an empty string
+// 3. write a loop in that reverses each letter
+// 4. put letter in empty string
+// 5. print the new string
